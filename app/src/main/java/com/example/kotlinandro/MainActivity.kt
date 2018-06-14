d78e80466c1b6af748faec9a7e7658e987c5b79f
nmalhotra <Neeraj.Malhotra@rsk-bsl.com>
2018-06-14 11:29:33 +0530

    first commit

---
+++ app/src/main/java/com/example/kotlinandro/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.kotlinandro
+
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        var button = findViewById<Button>(R.id.clickButton)
+
+
+    }
+
+    fun ClickMe(v: View) {
+
+
+        //Toast.makeText(this,"ClickMe",Toast.LENGTH_SHORT).show()
+       // print("Hello World")
+
+
+        val builder = AlertDialog.Builder(this@MainActivity)
+
+        // Set the alert dialog title
+        builder.setTitle("App background color")
+
+        // Display a message on alert dialog
+        builder.setMessage("Are you want to set the app background color to RED?")
+
+        // Set a positive button and its click listener on alert dialog
+        builder.setPositiveButton("YES"){dialog, which ->
+            // Do something when user press the positive button
+            Toast.makeText(applicationContext,"Ok, we change the app background.",Toast.LENGTH_SHORT).show()
+
+            // Change the app background color
+           // root_layout.setBackgroundColor(Color.RED)
+        }
+
+
+        // Display a negative button on alert dialog
+        builder.setNegativeButton("No"){dialog,which ->
+            Toast.makeText(applicationContext,"You are not agree.",Toast.LENGTH_SHORT).show()
+        }
+
+
+        // Display a neutral button on alert dialog
+        builder.setNeutralButton("Cancel"){_,_ ->
+            Toast.makeText(applicationContext,"You cancelled the dialog.",Toast.LENGTH_SHORT).show()
+        }
+
+        // Finally, make the alert dialog using builder
+        val dialog: AlertDialog = builder.create()
+
+        // Display the alert dialog on app interface
+        dialog.show()
+
+
+    }
+
+    }
